21ada37f135520a2526a8d210b3952f1bfb360e4
ksobi4 <filip456781337@gmail.com>
2023-07-02 10:07:38 +0200

    ENV

--- src/antisleeper.ts
+++ src/antisleeper.ts
@@ -5,11 +5,11 @@ import { load } from 'ts-dotenv';
 const env = load({
     PORT: Number,
     BASE_URL: String,
-    env: String
+    ENV: String
 });
 
 export async function callYourself() {
 
-    let url = env.env == 'DEV' ? `${env.BASE_URL}:${env.PORT}/call` : `${env.BASE_URL}/call`
+    let url = env.ENV == 'DEV' ? `${env.BASE_URL}:${env.PORT}/call` : `${env.BASE_URL}/call`
     await axios.get(url)
 }
